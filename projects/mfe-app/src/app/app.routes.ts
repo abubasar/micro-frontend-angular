a4f274b528ac518719f83849624d79e9067c39d6
Md.Ariful Islam <ariful.sec@gmail.com>
2024-12-13 10:47:13 +0600

    micro frontend implementation

---
+++ projects/mfe-app/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
+export const routes: Routes = [
+    {path:"",redirectTo:"todo-mfe",pathMatch:"full"},
+    {path:"todo-mfe",component:TodoListComponent}
+];
---
+++ projects/mfe-app/src/app/todo-list/todo-list.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TodoListComponent } from './todo-list.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path:"",component:TodoListComponent,pathMatch:"full"}
+    ])
+
+  ]
+})
+export class TodoListModule { }
